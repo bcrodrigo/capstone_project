02c76e6f34ef5398a5e3242ff7609e92143bd601
RodrigoBC <bcrodrigo16@gmail.com>
2024-03-26 21:29:01 -0700

    update to dataset_properties.py and 02_Dataloader_02dataset.ipynb

--- src/data/dataset_properties.py
+++ src/data/dataset_properties.py
@@ -1,7 +1,8 @@
 from torch.utils.data import ConcatDataset
 import torch
-import torchvision
 import torchvision.transforms as transforms
+from torchvision.transforms import v2
+
 
 def dataset_stat_prop(dataset):
     """Function to calculate the statistical properties of a custom dataset
@@ -17,20 +18,14 @@ def dataset_stat_prop(dataset):
         mean and standard deviation of all images in dataset
     """
 
-    # Normalize the images to the [0,1] interval
-    # transform = transforms.Compose([transforms.ToTensor()])
-
-    # Make sure the dataset has no transform
-    # dataset.transform = None
+    # Convert dataset into floating point otherwise it can't calculate the mean and std
+    dataset.transform = transforms.v2.ToDtype(torch.float)
     
     # stack all images together into a tensor of shape 
     # (N_images, 3, Height, Width)
     # note `sample` is a tuple with (tensor, label)
     
     x = torch.stack([sample[0] for sample in ConcatDataset([dataset])])
-
-    # convert to floating point, otherwise can't calculate the mean and std
-    x = x.to(torch.float)
 
     # Get mean and standard deviation. Note we're getting the statistical properties
     # along all images (dim 0), all heights (dim 2) and all widths (dim 3) leaving
