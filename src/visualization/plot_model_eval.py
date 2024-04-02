a02f31c453b3747cc1692f8f1f39ff4554b13850
RodrigoBC <bcrodrigo16@gmail.com>
2024-04-01 21:26:41 -0700

    updated plot_model_eval.py to include confusion matrix calculation

--- src/visualization/plot_model_eval.py
+++ src/visualization/plot_model_eval.py
@@ -1,5 +1,11 @@
+# to make plots related to model evaluation
 import numpy as np
 import matplotlib.pyplot as plt
+
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+
+from sklearn.metrics import roc_curve, roc_auc_score
 
 def plot_loss_acc(history):
     '''Function for plotting training and validation losses and accuracies
@@ -50,3 +56,80 @@ def plot_loss_acc(history):
     plt.grid()
     
     plt.show()
+
+
+def calculate_conf_matrix(label_list,pred_list,model_name,img_classes,savefig = False):
+    """Function to calculate the Confusion Matrix and the classification
+    report. 
+    
+    Parameters
+    ----------
+    label_list : list or array
+        True labels
+    pred_list : list or array
+        Predictions from the model
+    model_name : string
+        Name of the model being evaluated
+    img_classes : list
+        Classes of the classification problem
+    savefig : bool, optional
+        To save figure (confusion matrix) or not
+    
+    Returns
+    -------
+    string
+        Classification Report
+    """
+
+    # calculate the confusion matrix 
+    cm = confusion_matrix(label_list, pred_list)
+    disp = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels = img_classes)
+
+    fig, ax = plt.subplots(figsize = (5,4))
+    disp = disp.plot(ax=ax, cmap = 'Blues')
+    plt.tight_layout()
+    plt.title(f'Confusion Matrix for {model_name}')
+    
+    if savefig:
+        plt.savefig(f'confusion_matrix_{model_name}.png')
+    
+    plt.show()
+
+    report = classification_report(label_list,pred_list)
+    print(report)
+
+    return report
+
+def plot_roc_curve(label_list,pred_list,model_name):
+    """Function to plot the ROC curve. It also calculates the AUC score.
+    
+    Parameters
+    ----------
+    label_list : list or array
+        True labels
+    pred_list : list or array
+        Predictions from the model
+    model_name : string
+        Name of the model being evaluated
+    
+    Returns
+    -------
+    number
+        The calculated AUC score.
+    """
+
+    fpr, tpr, _ = roc_curve(label_list,pred_list)
+    score = roc_auc_score(label_list,pred_list)
+
+    plt.figure()
+    plt.plot(fpr,tpr, color = 'b', lw = 1, label = f'AUC {score:0.4f}')
+    plt.plot([0, 1], [0, 1], color='k', lw=1, linestyle='--')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+
+    plt.grid()
+    plt.title(f'ROC for {model_name}')
+    plt.legend()
+    plt.show()
+
+    return score
