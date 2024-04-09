9e2c3e985e0470fa77934019796da6fe27b964d9
RodrigoBC <bcrodrigo16@gmail.com>
2024-04-08 20:06:09 -0700

    added resnet34 to model_transfer_learning.py

--- src/models/model_transfer_learning.py
+++ src/models/model_transfer_learning.py
@@ -62,3 +62,32 @@ def resnet18_transfer_learning(N_CLASSES):
     resnet18.fc = nn.Linear(in_features = 512, out_features = N_CLASSES, bias = True)
 
     return resnet18
+
+def resnet34_transfer_learning(N_CLASSES):
+    """Function to create an instance of ResNet34 suitable for transfer learning 
+    of classification tasks
+    
+    Parameters
+    ----------
+    N_CLASSES : integer
+        Number of classes for the classification task. Must be >= 2
+    
+    Returns
+    -------
+    Object
+        PyTorch model (ResNet34) ready to be trained on the last layer only
+    """
+    assert isinstance(N_CLASSES,int),'N_CLASSES must be an integer'
+    assert (N_CLASSES >= 2),'N_CLASSES must be >= 2'
+
+    # download weights
+    resnet34 = models.resnet34(weights = 'DEFAULT')
+
+    # freeze model parameters for training
+    for param in resnet34.parameters():
+        param.requires_grad = False
+
+    # update the last layer to output N_CLASSES
+    resnet34.fc = nn.Linear(in_features = 512, out_features = N_CLASSES, bias = True)
+
+    return resnet34
